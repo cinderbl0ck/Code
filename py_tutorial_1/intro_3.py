ec805043a6cb5b0ed7755de24240957c1e923f7a
James Sanford <tempestofworlds@gmail.com>
2019-08-23 14:18:21 -0500

    completed tutorial 3 on Lists, Tuples, and Sets

--- py_tutorial_1/intro_3.py
+++ py_tutorial_1/intro_3.py
@@ -1,4 +1,4 @@
-# https://www.youtube.com/watch?v=W8KRzm-HUcc
+#https://www.youtube.com/watch?v=W8KRzm-HUcc
 # Lists start at 0 for index position
 courses = ['History', 'Math', 'Physics', 'CompSci']
 courses_2 = ['Art', 'Education']
@@ -32,16 +32,60 @@ nums = [1, 5, 2, 4, 3]
 # for item in courses:
 #	print(item)
 
-for index, course in enumerate(courses, start=1):
-	print(index, course)
+#for index, course in enumerate(courses, start=1):
+#	print(index, course)
 
+# turn list into string of csv
+#course_str = ', '.join(courses)
 
+#new_list = course_str.split(' - ')
+#print(new_list) 
 
+# Mutable
+#list_1 = ['History', 'Math', 'Physics', 'CompSci']
+#list_2 = list_1
 
+#print(list_1)
+#print(list_2)
 
+#list_1[0] = 'Art'
 
+#print(list_1)
+#print(list_2)
 
+#Immutable
+#tuple_1 = ('History', 'Math', 'Physics', 'CompSci')
+#tuple_2 = tuple_1
 
+#print(tuple_1)
+#print(tuple_2)
+
+#tuple_1[0] = 'Art'
+
+#print(tuple_1)
+#print(tuple_2)
+
+# Sets
+#cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
+#art_courses = {'History', 'Math', 'Art', 'Design'}
+
+#print(cs_courses.intersection(art_courses))
+#print('Math' in cs_courses)
+#print(cs_courses.difference(art_courses))
+
+#print(cs_courses.union(art_courses))
+
+#Empty Lists
+empty_list = []
+empty_list = list()
+
+#Empty Tuples
+empty_tuple = ()
+empty_tuple = tuple()
+
+#Empty Sets
+empty_set = {} # This isn't right! It's a dict
+empty_set = set() #<--- use this one
 
 
 
