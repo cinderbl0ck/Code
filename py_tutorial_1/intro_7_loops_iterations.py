7f864299a4bd63715cf10b46daf6edd3debe4349
James Sanford <tempestofworlds@gmail.com>
2019-08-23 15:04:43 -0500

    completed 7 loops and iterations   for while loops

---
+++ py_tutorial_1/intro_7_loops_iterations.py
@@ -0,0 +1,34 @@
+#https://www.youtube.com/watch?v=6iF8Xb7Z3wQ
+
+nums = [1, 2, 3, 4, 5]
+
+#for num in nums:
+#	if num == 3:
+#		print('Found!')
+#		#break
+#		continue
+#	print(num)
+
+
+#for num in nums:
+#	for letter in 'abc':
+#		print(num, letter)
+
+
+#for i in range(1, 11):
+#	print(i)
+
+
+x = 0
+
+#while x < 10:
+#	if x == 5:
+#		break
+#	print(x)
+#	x += 1
+
+while True: #This is for infinite loop break is needed or control + c 
+	if x == 5:
+		break
+	print(x)
+	x += 1 
