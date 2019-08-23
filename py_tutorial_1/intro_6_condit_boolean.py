ea8d6c799586672a7bde9694ed60f32ca755009b
James Sanford <tempestofworlds@gmail.com>
2019-08-23 14:53:03 -0500

    completed conditionals and booleans if else and elif

---
+++ py_tutorial_1/intro_6_condit_boolean.py
@@ -0,0 +1,73 @@
+#https://www.youtube.com/watch?v=DZwmZ8Usvnk
+
+#Comparisons:
+# Equal:            ==
+# Not Equal:        !=
+# Greater Than:     >
+# Less Than:        <
+# Greater or Equal: >=
+# Less or Equal:    <=
+# Object Identity:  is   
+
+# and
+# or
+# not
+
+#language = 'Java'
+
+#if language == 'Python':
+#	print('Language is Python')
+#elif language == 'Java':
+#	print('Language is Java')
+#elif language == 'JavaScript':
+#	print('Language is JavaScript')
+#else:
+#	print('No match')
+
+
+
+
+#user = 'Admin'
+#logged_in = False
+
+#if user == 'Admin' or logged_in:
+#	print('Admin Page')
+#else:
+#	print('Bad Creds')
+
+
+#if not logged_in:
+#	print('Please Log In')
+#else:
+#	print('Welcome')
+
+
+#a = [1,2,3]
+#b = [1,2,3]
+#b = a
+
+#print(id(a))
+#print(id(b))
+#print(a == b)
+#print(a is b)
+#print(id(a) == id(b))
+
+
+
+#False Values:
+	# False
+	# None
+	# Zero of any numeric type
+	# Any empty sequence.  For example, '', (), [].
+	# Any empty mapping.  For example, {}.
+
+condition = {}
+
+if condition:
+	print('Evaluated to True')
+else:
+	print('Evaluated to False')	
+
+
+
+
